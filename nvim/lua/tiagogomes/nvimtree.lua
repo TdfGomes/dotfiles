217f254503fbd3c6c222ad22df6ef6e52155fea7
Tiago Gomes <tiago.gomes@farfetch.com>
2021-09-24 09:48:39 +0100

    chore: add nvim tree icons and open tree on file location

--- nvim/lua/tiagogomes/nvimtree.lua
+++ nvim/lua/tiagogomes/nvimtree.lua
@@ -3,9 +3,10 @@ vim.cmd("autocmd Colorscheme * highlight NvimTreeNormal guibg=#242b3")
 vim.g.nvim_tree_auto_open = 1
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_quit_on_open = 1
-
+vim.g.nvim_tree_follow = 1 
+vim.g.nvim_tree_follow_update_path = 1
 vim.g.nvim_tree_icons = {
-  git_icons = {
+  git = {
     unstaged = " ",
     staged =  " ",
     renamed =  " ",

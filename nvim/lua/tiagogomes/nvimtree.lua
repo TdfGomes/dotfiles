6ea5e58a664ff1c15a65484d0119d310dee31bdd
Tiago Gomes <tiago.gomes@farfetch.com>
2021-10-01 09:41:12 +0100

    chore: update nvimtree configurations

--- nvim/lua/tiagogomes/nvimtree.lua
+++ nvim/lua/tiagogomes/nvimtree.lua
@@ -1,10 +1,6 @@
 vim.cmd("autocmd Colorscheme * highlight NvimTreeNormal guibg=#242b3")
 
-vim.g.nvim_tree_auto_open = 1
-vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_quit_on_open = 1
-vim.g.nvim_tree_follow = 1 
-vim.g.nvim_tree_follow_update_path = 1
 vim.g.nvim_tree_icons = {
   git = {
     unstaged = " ",

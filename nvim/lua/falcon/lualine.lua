0a5dcc3cccbe3ae96a1e752663f4782ed93c8384
TdfGomes <t.dfgomes@icloud.com>
2021-12-15 16:26:25 +0000

    chore: update lualine configs to match new plugin requirements

--- nvim/lua/falcon/lualine.lua
+++ nvim/lua/falcon/lualine.lua
@@ -1,11 +1,4 @@
-local c = {
-  green = "#97ca72",
-  red = "#ef5f6b",
-  blue = "#5ab0f6",
-  purple = "#ca72e4",
-  bg3 = "#363c51",
-  yellow = "#ebc275"
-}
+local c = require('falcon.colors').c 
 
 require('lualine').setup {
   options = {

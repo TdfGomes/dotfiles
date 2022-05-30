0eb265560cb11f55b30aed304c4e5ad6d2b938e9
TdfGomes <t.dfgomes@icloud.com>
2022-05-30 10:47:44 +0100

    feat(lsp-cmp): map TAB to insert the autocomplete suggestion

--- nvim/lua/falcon/lsp.lua
+++ nvim/lua/falcon/lsp.lua
@@ -46,7 +46,7 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping.confirm({ select = true }),
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },

a03cb9fde65c819b8b047ff7384727c2a80c15eb
TdfGomes <t.dfgomes@icloud.com>
2022-08-29 11:09:08 +0100

    chore: update lsp configs to be compliant with the latest changes

--- nvim/lua/falcon/lsp.lua
+++ nvim/lua/falcon/lsp.lua
@@ -19,7 +19,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
 
-  if client.resolved_capabilities.document_formatting then
+  if client.server_capabilities.document_formatting then
     vim.api.nvim_command [[augroup Format]]
     vim.api.nvim_command [[autocmd! * <buffer>]]
     vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
@@ -78,13 +78,14 @@ for type, icon in pairs(signs) do
 end
 
 -- setup servers
-local servers = { 'tsserver', 'eslint' }
+local servers = { 'tsserver', 'eslint'}
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
     handlers = handlers,
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    capabilities = capabilities,
     root_dir = vim.lsp.buf.list_workspace_folders(),
     root_dir = nvim_lsp.util.root_pattern('.git'),
     commands = {
@@ -93,6 +94,32 @@ for _, lsp in ipairs(servers) do
         description = 'Organize Imports'
       }
     }
-  }
-end
+  }    
+end    
 
+--Rust
+nvim_lsp.rust_analyzer.setup {
+  -- on_attach = on_attach,
+  capabilities = capabilities,
+  handlers = handlers,
+  settings = {
+    ["rust-analyzer"] = {
+      assist = {
+        importMergeBehavior = "last",
+        importPrefix = "by_self",
+      },
+      diagnostics = {
+        disabled = { "unresolved-import" }
+      },
+      cargo = {
+          loadOutDirsFromCheck = true
+      },
+      procMacro = {
+          enable = true
+      },
+      -- checkOnSave = {
+      --     command = "clippy"
+      -- },
+    }
+  }
+}

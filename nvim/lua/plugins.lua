285f17ca42b6efd0f3089288b5a9886c618477cb
TdfGomes <t.dfgomes@icloud.com>
2024-11-12 13:48:57 +0000

    chore: update plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -18,7 +18,13 @@ return require('packer').startup(function()
   }
   -- Snipet engine
   use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip'
+  use({
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!:).
+    run = "make install_jsregexp"
+  })
   -- Telescope
   use 'nvim-lua/plenary.nvim'
   use 'nvim-telescope/telescope.nvim'
